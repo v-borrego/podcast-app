408ca4f87bdf254827a93353eb4a4c93f6bd4b7a
Víctor Borrego Pérez <vborrego@b2bsalud.com>
2019-04-12 12:45:29 +0200

    Import uppercase name issue fixed

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 import VueRouter from "vue-router";
 
-import App from "./App.vue";
+import App from "./app.vue";
 import { router } from "./router";
 import "./assets/css/global-style.css";
 
